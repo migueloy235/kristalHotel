04bb34e4cc7aadf07360cd8ed3ef655274ae0598
blerome <105025286+blerome@users.noreply.github.com>
2023-05-26 23:46:27 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
 from miapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name="index"),
+    path('index/',views.index, name="index"),
+    path('',views.login, name="login"),
+    path('crear-formulario/', views.crear_formulario, name="crear-formulario"),
+    path ('save/', views.save_folio , name ="save"),
 ]
