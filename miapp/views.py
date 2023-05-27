8a7ea739461286c44407f1011c8be8b94a2d4adf
blerome <105025286+blerome@users.noreply.github.com>
2023-05-26 23:45:52 -0500

    Update views.py

--- miapp/views.py
+++ miapp/views.py
@@ -12,3 +12,19 @@ def index(request):
 def login(request):
 
     return render(request, 'login.html')
+
+def crear_formulario(request):
+
+    return render(request,'articulos.html')
+
+def save_folio (request):
+    folios = Folios(
+        
+        area = area,
+        objeto = objeto,
+        valor = valor,
+        contenido = content,
+
+    )
+    folios.save()
+    return HttpResponse("Se envio tu solicitud")
